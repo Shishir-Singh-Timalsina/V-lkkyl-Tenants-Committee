3a6a62bfeac2987fe1ad508fbb9f869e35842446
Shishir-Singh-Timalsina <shishir.singh.timsina@gmail.com>
2019-04-21 17:05:16 +0300

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class header extends Component {
+  render() {
+      return(
+        <h2>Välkkylä Tenants Committee</h2>
+
+      );
+  }
+}
+
+export default header;
